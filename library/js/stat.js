4f4e6b65e70a1f8851aae0eb9465d7fc92f8a0bf
Vanadiry <vanadiry@yeah.net>
2022-01-16 23:30:50 +0800

    Site updated: 2022-01-16 23:30:49

---
+++ library/js/stat.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?72d912cf5dbefca5d8e9a04276ab7a66";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
